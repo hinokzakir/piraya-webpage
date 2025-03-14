b50c7bad6671f4cea6721b2dadbef65082ba4f0b
hinokzakir <hinokzakir@gmail.com>
2025-03-14 19:54:36 +0100

    Vue frontend setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" width="100px" height="100px" src="./assets/logo.png">
+  <HelloWorld msg="Under construction...Ernst has eaten too much of it."/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
